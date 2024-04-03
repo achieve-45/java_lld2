56e9afd012395c0a0b23f0c2b666444f15cb8ff7
achieve-45 <pupuachieve7@gmail.com>
2024-04-03 23:26:03 +0530

    yes

--- src/AccessModifiers/Client.java
+++ src/AccessModifiers/Client.java
@@ -5,7 +5,7 @@ import IntroToOOPs.Student;
 public class Client {
     public static void main(String[] args) {
         Student s = new Student();
-        s.name = "Akash";
+        s.name = "Achieve";
 //        s.id = 10; // private can't be accessed outside class
 //        s.age = 45;
         Student y = new Student();
